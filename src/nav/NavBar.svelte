dd6a58417459bc59e3233f6202082f6cc9203a3d
Julian Laury <julian.laury@hotmail.com>
2023-04-14 22:55:01 -0400

    Throw colors at the wall to see if they stick Set up routing Run around the world Trying to make my react mind do svelte instead of pretending like svelte is react

---
+++ src/nav/NavBar.svelte
@@ -0,0 +1,10 @@
+<script>
+
+  import { navBar } from "./NavBar";
+
+</script>
+<div class={navBar}>
+  <a href="/">Home</a>
+  <a href="/scanRecipe">Scan a Recipe</a>
+  <a href="/about">About</a>
+</div>
